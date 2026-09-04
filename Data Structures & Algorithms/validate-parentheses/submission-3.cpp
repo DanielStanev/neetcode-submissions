9841744370532a079c71fe7a134517b07a458719
Didko <62813898+DanielStanev@users.noreply.github.com>
2026-09-04 10:26:42 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        char stack[s.size()];
+        size_t head = 0;
+
+        for (const char& x : s) {
+            switch (x) {
+                case '(': 
+                case '{': 
+                case '[':
+                    stack[head++] = x;
+                    break;
+                case ')':
+                    if (!head || stack[--head] != '(')
+                        return false;
+                    break;
+                case '}':
+                    if (!head || stack[--head] != '{')
+                        return false;
+                    break;
+                case ']':
+                    if (!head || stack[--head] != '[')
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        return !head;
+    }
+};
